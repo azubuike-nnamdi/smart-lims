c02a4c90f8d1c260991a1035d09f6f3632397e35
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-24 22:19:19 +0100

    feat: config routes :racehorse:

---
+++ config/routes.ts
@@ -0,0 +1,20 @@
+
+
+
+//AUTH ROUTE
+const LOGIN_URL: string = "/auth/login"
+const REGISTER_URL: string = "/auth/register"
+const VERIFY_OTP_URL: string = "/auth/verify-otp"
+const FORGOT_PASSWORD_URL: string = "/auth/forgot-password"
+const RESET_PASSWORD_URL: string = "/reset-password"
+
+
+// Back Office
+
+export {
+  LOGIN_URL,
+  REGISTER_URL,
+  VERIFY_OTP_URL,
+  FORGOT_PASSWORD_URL,
+  RESET_PASSWORD_URL,
+}
